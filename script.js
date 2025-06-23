be9dc38dd18ae1ea9065c118bcfdc052d9f992ed
dharmagunabkl <sentradharmagunabengkulu@gmail.com>
2025-06-23 13:54:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,13 +18,4 @@ const daftarPegawai = [
 
 function login() {
   const inputNama = document.getElementById("inputNama").value.trim();
-  const inputPassword = document.getElementById("inputPassword").value;
-  const error = document.getElementById("error");
-
-  if (daftarPegawai.includes(inputNama) && inputPassword === "Salemba@28") {
-    localStorage.setItem("namaPegawai", inputNama);
-    window.location.href = "dashboard.html";
-  } else {
-    error.textContent = "❌ Nama atau password salah.";
-  }
-}
+  const inputPassword = document.getElementById("inputPassword").value.trim();
